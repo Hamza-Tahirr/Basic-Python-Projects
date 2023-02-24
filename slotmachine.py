1f3fd5edd8c0ddb39e4e75e9895efa9bda1f302a
Hamza Tahir <90556877+Hamza-Tahirr@users.noreply.github.com>
2023-02-24 12:40:00 +0500

    Add files via upload

--- slotmachine.py
+++ slotmachine.py
@@ -1,3 +1,8 @@
+# Defining global constant that is maximum number of lines to bet on
+MAX_LINES = 3
+MAX_BET = 10000
+MIN_BET = 1
+
 #Defining a funtion for depositing the money
 def money_deposit():
     while True:
@@ -11,6 +16,29 @@ def money_deposit():
         else:
             print("Enter amount number only")
 
-    return amount            
-         
-money_deposit()
+    return amount        
+
+#For betting on number of lines 
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines you want to bet on (1-" + str(MAX_LINES) + ")? : ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a valid number of lines")
+        else:
+            print("Enter number only")
+
+    return lines  
+
+
+
+
+def main():         
+    balance = money_deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()    
